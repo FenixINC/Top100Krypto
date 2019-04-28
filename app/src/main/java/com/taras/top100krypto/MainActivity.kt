e87e93726500e4217a827eeb0f1f38b0c353ce63
Taras Koloshmatin <taras@pier2marketing.com>
2019-04-28 21:50:53 +0300

    Main, About activities; AdMob banners; migrated to androidx

--- app/src/main/java/com/taras/top100krypto/MainActivity.kt
+++ app/src/main/java/com/taras/top100krypto/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.taras.top100krypto
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
+import com.taras.top100krypto.activities.AboutActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mInterstitialAd: InterstitialAd
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-3278404235019235/5116950068"
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+    }
+
+    private fun showAd() {
+        if (mInterstitialAd.isLoaded) {
+            mInterstitialAd.show()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        showAd()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.action_about -> {
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/taras/top100krypto/activities/AboutActivity.kt
+++ app/src/main/java/com/taras/top100krypto/activities/AboutActivity.kt
@@ -1,4 +1,26 @@
 package com.taras.top100krypto.activities
 
-class AboutActivity {
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.taras.top100krypto.R
+import kotlinx.android.synthetic.main.activity_about.*
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        btn_rate_app.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${applicationContext.packageName}")))
+        }
+
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+    }
 }
